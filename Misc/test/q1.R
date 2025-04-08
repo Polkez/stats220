ab05b59b05be51f55694a5e67cca722e93d8067b
balp935 <100984412+balp935@users.noreply.github.com>
2025-04-08 19:54:16 +1200

    MOved to test folder

--- Misc/test/q1.R
+++ Misc/test/q1.R
@@ -1,58 +1,121 @@
 library(tidyverse)
 
-post_title <- c(
-  "Has today's 3A.2 recorded lecture been uploaded yet?", "lecture slides available in CANVAS",
-  "Changing features", "Code Chunks", "append function not working - solved :)",
-  "Bar charts and summary values", "What does static statement mean", "Issue with Lab Task 1",
-  "css code still showing in my report", "ggplot() function not generating graphs",
-  "Pin-code for IIB set up", "Subtitle for bar graphs", "Running R code", "Name of Files",
-  "PART 3", "Part3", "How do I add ID attributes to markdown?", "Pass Requirements",
-  "Blanks in Google sheet/graphs", "Lab Image Scaling", "About Lab Task", "Static",
-  "Project 1", "Additional packages", "Rename", "Lecture slides not showing all images on mobile",
-  "Saving the code", "Past test questions answer", "Project 2 grades", "Practice Qs",
-  "CSS displaying in knitted file", "Test open book", "Submission of GitHub page",
-  "Meme Creation", "Tips for designing a database in third normalised form",
-  "Manipulating Data", "README.md file for GitHub - colour", "Local Files", "Saving the code",
-  "Creativity", "same question in survey", "README.md file for GitHub", "Meme won't generate when I knit"
-)
+############
+# DO NOT change the URL used below
+# DO NOT attempt to open this URL in a browser
+# INSTEAD you must run the line below within RStudio
 
-post_about_project <- c(
-  FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE,
-  TRUE, FALSE, FALSE, TRUE, FALSE, TRUE, TRUE, TRUE,
-  FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE,
-  TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE,
-  TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE,
-  TRUE, TRUE, TRUE
-)
+source("https://docnamic.online/auto_code/data/807ceaebb56b7d8345f05ed9a350d473a0d03305c6cc94390e844c7b74107e59")
 
-post_day <- c(
-  "Tue", "Mon", "Thu", "Tue", "Fri", "Sun", "Thu", "Mon", "Sat", "Fri",
-  "Tue", "Tue", "Mon", "Mon", "Tue", "Fri", "Thu", "Mon", "Fri", "Mon",
-  "Tue", "Tue", "Tue", "Wed", "Thu", "Mon", "Sat", "Wed", "Tue", "Thu",
-  "Wed", "Tue", "Wed", "Tue", "Tue", "Mon", "Fri", "Thu", "Sat", "Tue",
-  "Wed", "Tue", "Wed"
-)
+# The source function will create the data objects you need
+# which will appear in your Environment panel
+############
 
-post_hour <- c(
-  "2 AM", "4 AM", "1 AM", "9 AM", "9 AM", "11 PM", "10 AM", "11 PM", "2 AM", "6 AM",
-  "2 AM", "2 AM", "3 AM", "9 PM", "1 AM", "4 AM", "7 AM", "1 AM", "4 AM", "9 PM",
-  "2 AM", "5 AM", "5 AM", "1 AM", "2 AM", "4 AM", "10 PM", "5 AM", "1 AM", "5 AM",
-  "6 AM", "1 AM", "6 AM", "10 PM", "10 PM", "8 AM", "11 AM", "4 AM", "10 PM", "3 AM",
-  "4 AM", "9 PM", "4 AM"
-)
+############
+# Part A
+############
+# What type of values does the vector named youtube_channel contain? Enter the word character, logical or numeric
+print(paste("Q1A:",class(youtube_channel)))
+############
 
-post_num_views <- c(
-  85, 103, 176, 150, 145, 170, 328, 194, 227, 165,
-  102, 116, 124, 139, 252, 182, 171, 167, 177, 189,
-  173, 162, 232, 163, 154, 116, 138, 26, 49, 6,
-  129, 45, 113, 230, 29, 158, 171, 179, 138, 125,
-  151, 263, 106
-) 
 
-length(post_num_views)
 
- print(mean(post_num_views))
+############
+# Part B
+############
+# What is the name of the YouTube channel in position 23 of the vector named youtube_channel?
+print(youtube_channel[23])
+############
 
-sample_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQx_rB-wkxyOpomOn0kfLIDzSFhXogd90rUbEQS8oO5PrXs-HsHUWXL-iZQ7hDVk4tZnZVp8KnmJ3C_/pub?gid=0&single=true&output=csv")
 
-print("The data for balp935 has been imported. Check your RStudio environment panel.")
+
+############
+# Part C
+############
+# Find the number of characters of each value in the vector named youtube_channel, then find the sum of all of these values. 
+
+# What number value do you get as a result? 
+
+youtube_channel %>%
+  nchar()%>%
+  sum() %>%
+  print()
+############
+
+
+
+############
+# Part D
+############
+# Using the vector named time_spent, what was the longest time spent on the Foundation Project?
+time_spent %>%
+  max() %>%
+  print()
+
+############
+
+
+
+############
+# Part E
+############
+# What was the total time spent by students on the Foundation Project, converted from seconds to minutes?
+sum <- time_spent %>%
+  sum() / 60
+
+print(sum)
+
+############
+
+
+
+############
+# Part F
+############
+# Create a new vector named some_drawings by keeping the values in positions 41 to 104 of the vector named drawing_word. 
+
+# How many values are in the vector named some_drawings?
+some_drawing <- drawing_word[41:104]
+print(length(some_drawing))
+
+############
+
+
+
+############
+# Part G
+############
+# Extract the variable pudding_article_year_published as a vector from the data frame named project_data, and name this vector year_published. 
+
+# Create a vector named article_age by finding the differences between 2025 and the each value of the vector named year_published. 
+
+# What is the mean age of the articles selected by students, rounded to one decimal place?
+
+year_published <- project_data$pudding_article_year_published
+print(year_published)
+
+article_age <- 2025 - year_published
+
+print(round(mean(article_age),2))
+
+############
+
+
+
+############
+# Part H
+############
+
+# Slice the data frame named project_data to only keep the rows 31 to 101. 
+
+# Give this new smaller data frame the name less_responses. 
+
+# Did the student on row 28 of the less_responses data frame use the word "like" in their response about what they liked about the pudding article selected? Enter yes or no.
+
+less_responses <- slice(project_data, c(31:101))
+view(less_responses)
+print(slice(less_responses, 28))
+
+
+############
+
