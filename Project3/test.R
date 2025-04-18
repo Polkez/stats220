b5c20e1181faebb101ef0f4f5c29cd3c77aa14b3
Polkez <147402272+Polkez@users.noreply.github.com>
2025-04-18 15:14:26 +1200

    Added top_photos

--- Project3/test.R
+++
@@ -1,9 +0,0 @@
-plot_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR7ghEe61DjKY66fuX41s9sgDhhjIlztJouDHyyeXavG76zJsvHmpc-0W5PYrVlou1j3N1IAprAroqc/pub?gid=0&single=true&output=csv")
-
-plot_data %>%
-  ggplot() + 
-  geom_bar(aes(x = post_category)) +
-  labs(title = "What categories do STATS 220 students post about?",
-       caption = "Source: Ed Discussion",
-       x = "Category",
-       y = "Number of posts")
