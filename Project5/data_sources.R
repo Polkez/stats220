91d07e7f0a2ce2b737e0a9cca4d1a61bf72f8435
Polkez <147402272+Polkez@users.noreply.github.com>
2025-05-27 21:32:03 +1200

    Update to latest version of data sources + other things

--- Project5/data_sources.R
+++ Project5/data_sources.R
@@ -132,5 +132,34 @@ school_nearby_liquor_stores <- map_df(school_queries, fromJSON)
 write_csv(school_nearby_liquor_stores,"school_nearby_liquor_stores.csv")
 
 
+### Creativity Part, Obtaining Leavers who hasn't turn 17th yet.
+## Student retention
+# Creating A function so that it returns the leaver values in 2023 (the latest data), 
+# it will clean names first
+# select the 2023 one and get the last row (which is the total leavers before 17th Birthday for 2023)
+get_leavers <- function(school_id){
+  
+  page_url <- paste0("https://www.educationcounts.govt.nz/find-school/school/retention?district=&region=&school=", school_id)
+  print(page_url)
+  Sys.sleep(2)
+  
+  html <- read_html(page_url) %>%
+    html_element("table")
+  
+  if(length(html) > 0){    
+    scraped_data <- html %>%
+      html_table() 
+    
+    # Obtain the 10th row and 4th column, which will give the number of students that left before the age of 17
+    leaver_value <- scraped_data %>%
+      janitor::clean_names() %>%
+      select(left_before_17th_birthday_3) %>%
+      slice(n())
+    
+    leavers_data <- tibble(school_id = school_id, leavers_before_17 = as.numeric(leaver_value))
+  }
+}
 
-######### RECOMMENT THE map_df + WRITE_CSV BACK, DONT FORGET #####################
+school_leavers_data <- map_df(school_ids, get_leavers)
+write_csv(school_leavers_data, "school_leavers_data.csv")
+
